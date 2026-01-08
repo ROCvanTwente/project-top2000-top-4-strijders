ff550f76e998effb8fc21b235fc8d13f66324af3
Salim <0357057@student.rocvantwente.nl>
2026-01-08 11:49:43 +0100

    Apicontroller aangepast voor het ophalen van top2000 jaar en een Apicontroller voor het ophalen van nummers

--- TemplateJwtProject/Controllers/GetTop2000EntriesController.cs
+++ TemplateJwtProject/Controllers/GetTop2000EntriesController.cs
@@ -19,61 +19,18 @@ namespace TemplateJwtProject.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Top2000Entry>>> Get100Entries(int year, int pagenition, string? artistName, bool allEntryOfYear = false)
+        public async Task<ActionResult<List<Top2000Entry>>> GetYearEntries(int year)
         {
-            int start = 1;
-            int end = 2000;
 			try
 			{
                 if (year >= 1999 && year < 2025)
                 {
-                    List<Top2000Entry> allEntries = new List<Top2000Entry>();
-                    
-                    if (allEntryOfYear)
-                    {
-                        allEntries = await _context.Top2000Entry
-                            .Where(s => s.Year == year)
-                            .Include(s => s.Songs)
-                            .ThenInclude(s => s.Artist)
-                            .OrderBy(s => s.Position)
-                            .ToListAsync(); 
-                        return Ok(allEntries);
-                    }
-                    
-                    if (pagenition <= 1)
-                    {
-                        start = 1;
-                        end = 100;
-                    }
-                    else if (pagenition <= 20)
-                    {
-                        start = (pagenition - 1) * 100;
-                        end = pagenition * 100;
-                    }
-                    else
-                    {
-                        Console.WriteLine("mag niet meer dan 20");
-                        return BadRequest("mag niet meer dan 20");
-                    }
-                    
-                    if (artistName == null)
-                    {
-                        allEntries = await _context.Top2000Entry
-                            .Where(s => s.Year == year && (s.Position >= start && s.Position <= end))
-                            .Include(s => s.Songs)
-                            .ThenInclude(s => s.Artist)
-                            .OrderBy(s => s.Position)
-                            .ToListAsync();
-                    }
-                    else
-                    {
-                        allEntries = await _context.Top2000Entry
-                            .Include(s => s.Songs)
-                            .ThenInclude(s => s.Artist)
-                            .Where(s => s.Year == year && s.Songs.Artist.Name == artistName)
-                            .OrderBy(s => s.Position)
-                            .ToListAsync();
-                    }
+                    List<Top2000Entry> allEntries = await _context.Top2000Entry
+                        .Where(s => s.Year == year)
+                        .Include(s => s.Songs)
+                        .ThenInclude(s => s.Artist)
+                        .OrderBy(s => s.Position)
+                        .ToListAsync(); 
                     return Ok(allEntries);
                 }
                 else
