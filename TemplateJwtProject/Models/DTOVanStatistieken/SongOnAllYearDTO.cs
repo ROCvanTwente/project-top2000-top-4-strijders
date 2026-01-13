23825f9c9b0bc91811b5b7232470b388c9f2990c
Salim <0357057@student.rocvantwente.nl>
2026-01-13 19:29:30 +0100

    StatistiekenController en al zijn models toegeveogd

---
+++ TemplateJwtProject/Models/DTOVanStatistieken/SongOnAllYearDTO.cs
@@ -0,0 +1,9 @@
+namespace TemplateJwtProject.Models.DTOVanStatistieken;
+
+public class SongOnAllYearDTO
+{
+    public int SongID { get; set; }
+    public string Title { get; set; }
+    public string ArtistName { get; set; }
+    public int ReleaseYear { get; set; }
+}
